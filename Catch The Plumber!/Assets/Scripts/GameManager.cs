94ca8c890d57826a4f447103639e65b8a8794fee
Declan Killmurray <dsk6842@g.rit.edu>
2025-02-19 13:46:13 -0500

    thing

--- Catch The Plumber!/Assets/Scripts/GameManager.cs
+++ Catch The Plumber!/Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame       a
     void Update()
     {
         
